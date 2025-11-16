dd8801b885505e2443d69631d6c209f62446625f
luidll <luisfelipemoraes32@gmail.com>
2025-11-16 13:23:40 -0300

    feat(DesktopApp): Configura Injeção de Dependência para o AppDbContext

--- Agendamento.DesktopApp/MauiProgram.cs
+++ Agendamento.DesktopApp/MauiProgram.cs
@@ -1,26 +1,40 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// Adicione estes 'usings' no topo do arquivo
+using Agendamento.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+// ---
 
-namespace Agendamento.DesktopApp;
+using Microsoft.Extensions.Logging;
 
-public static class MauiProgram
+namespace Agendamento.DesktopApp
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
 
-		builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+            const string connectionString = "Server=localhost\\SQLEXPRESS;Database=AgendamentoDB;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=True;";
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+
+
+            return builder.Build();
+        }
+    }
 }
